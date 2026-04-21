244b4d1acea3608854e1ee437a1f5dcf7b893240
vargas <vargascahyadi@gmail.com>
2026-04-21 17:23:58 +0700

    membuat file app_router

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,24 @@
+import '../../main.dart';
+import 'package:flutter/material.dart';
+import '../../features/auth/presentation/pages/login_page.dart';
+import '../../features/auth/presentation/pages/register_page.dart';
+import '../../features/auth/presentation/pages/email_page.dart';
+import '../../features/dasboard/presentation/pages/dashboard_page.dart';
+import '../guards/auth_guard.dart';
+
+class AppRouter {
+  static const String splash      = '/';
+  static const String login       = '/login';
+  static const String register    = '/register';
+  static const String verifyEmail = '/verify-email';
+  static const String dashboard   = '/dashboard';
+
+
+  static Map<String, WidgetBuilder> get routes => {
+    splash:      (_) => const SplashPage(),
+    login:       (_) => const LoginPage(),
+    register:    (_) => const RegisterPage(),
+    verifyEmail: (_) => const VerifyEmailPage(),
+    dashboard:   (_) => const AuthGuard(child: DashboardPage()),
+  };
+}
