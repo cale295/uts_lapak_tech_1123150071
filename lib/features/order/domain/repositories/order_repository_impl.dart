9289a335c68220bcea67b25b2d5d946bd6fbf928
vargas <vargascahyadi@gmail.com>
2026-05-21 23:20:56 +0700

    membuat order_repository_impl

---
+++ lib/features/order/domain/repositories/order_repository_impl.dart
@@ -0,0 +1,37 @@
+import '../../data/model/order_model.dart';
+import '../../data/repositories/order_repository.dart';
+import '../../../../core/constants/api_constants.dart';
+import '../../../../core/services/dio_client.dart';
+
+class OrderRepositoryImpl implements OrderRepository {
+
+
+  @override
+  Future<OrderModel> checkout({
+    required String shippingAddress,
+    String? notes,
+    required String paymentMethod,
+  }) async {
+    final response = await DioClient.instance.post(
+      ApiConstants.checkout,          // POST /v1/orders/checkout
+      data: {
+        'shipping_address': shippingAddress,
+        'notes': notes ?? '',
+        'payment_method': paymentMethod,
+      },
+    );
+    final data = response.data['data'] as Map<String, dynamic>;
+    return OrderModel.fromJson(data);
+  }
+
+
+  @override
+  Future<List<OrderModel>> getMyOrders({int page = 1, int limit = 10}) async {
+    final response = await DioClient.instance.get(
+      ApiConstants.orders,
+      queryParameters: {'page': page, 'limit': limit},
+    );
+    final List<dynamic> data = response.data['data'] ?? [];
+    return data.map((e) => OrderModel.fromJson(e)).toList();
+  }
+}
